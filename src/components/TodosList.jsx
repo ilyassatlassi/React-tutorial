ceb550b21ea845dc5dfc2d74a7ef57dcdf91ce29
ilyass atlassi <atlassiilyass@gmail.com>
2023-03-30 21:26:49 +0000

    Add files

---
+++ src/components/TodosList.jsx
@@ -0,0 +1,21 @@
+import TodoItem from '@/components/TodoItem';
+
+const TodosList = ({
+  todosProps,
+  handleChange,
+  delTodo,
+  setUpdate,
+}) => (
+  <ul>
+    {todosProps.map((todo) => (
+      <TodoItem
+        key={todo.id}
+        itemProp={todo}
+        handleChange={handleChange}
+        delTodo={delTodo}
+        setUpdate={setUpdate}
+      />
+    ))}
+  </ul>
+);
+export default TodosList;
